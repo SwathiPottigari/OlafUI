2a4af2e03b7aecc673f1642ac5bedcda5902132a
Shivalibhalla <shivalibhalla@gmail.com>
2019-11-01 16:01:29 -0700

    markers with sample data

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,4 +1,91 @@
 import React, { Component } from "react";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+  InfoWindow
+} from "react-google-maps";
+
+import onlineChefData from "./chef.js"
+
+require('dotenv').config()
+
+
+const MapWithAMarker = withScriptjs(withGoogleMap( props =>{
+
+  console.log(onlineChefData)
+     return (
+         <GoogleMap
+         defaultZoom={12}
+         defaultCenter={props.location}
+        /*  defaultOptions={{ styles: mapStyles }} */
+       >
+
+       {onlineChefData.map(chef => (
+        <Marker
+          position={{
+            lat: parseFloat(chef.Chef.lat),
+            lng: parseFloat(chef.Chef.lng)
+          }}
+          /* onClick={() => {
+            setSelectedChef(chef);
+          }} */
+           icon={{
+            url:  require("./pepper.svg"),
+            scaledSize: new window.google.maps.Size(40, 60) 
+          }}
+         
+
+        />
+      ))}
+         
+       </GoogleMap>
+     )
+ }
+ ));
+
+
+
+
+class Map extends Component {
+  state = {
+    location: null    
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(position => {
+      let currentLoc = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      }
+      this.setState({
+        location: currentLoc
+      })
+    })
+  }
+  render() {
+    return <div style={{ width: "100vw", height: "100vh" }}>
+      
+      {this.state.location?(<MapWithAMarker
+        location={this.state.location}
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY
+        }`}
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `100%` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />):(
+        <h1>LOADING.......</h1>
+      )}
+    </div>
+  }
+}
+
+export default Map;
+
+/* 
+
+import React, { Component } from "react";
 
 import {
   withScriptjs,
@@ -6,14 +93,14 @@ import {
   GoogleMap,
   Marker,
 } from "react-google-maps";
-/* import mapStyles from "./mapStyles"; */
+
 require('dotenv').config()
 
 const MapWithAMarker = withScriptjs(withGoogleMap(props =>
   <GoogleMap
     defaultZoom={12}
     defaultCenter={props.location}
-   /*  defaultOptions={{ styles: mapStyles }} */
+  
   >
     <Marker
       position={{ lat: 47.674, lng: -122.124 }}
@@ -57,4 +144,4 @@ class Map extends Component {
   }
 }
 
-export default Map;
+export default Map; */
---
+++ src/components/Map/chef.js
@@ -0,0 +1,48 @@
+const onlineChefData =  [
+    {
+        "id": 1,
+        "createdAt": "2019-11-01T10:58:01.000Z",
+        "updatedAt": "2019-11-01T10:58:01.000Z",
+        "ChefId": 2,
+        "Chef": {
+            "id": 2,
+            "firstName": "Shivali",
+            "lastName": "Chopra",
+            "email": "test1@gmail.com",
+            "specialities": "Rajmah Chawal",
+            "contact": "+14255536595",
+            "kitchenName": "Punjabi Tadka",
+            "license": "WA-FOODLICENSE",
+            "lat":"47.6",
+            "lng":"-122.33",
+            "address": "400 Broad St, Seattle, WA 98109",
+            "createdAt": "2019-10-31T22:03:04.000Z",
+            "updatedAt": "2019-10-31T22:03:04.000Z",
+            "UserId": 3
+        }
+    },
+    {
+        "id": 2,
+        "createdAt": "2019-11-01T10:58:01.000Z",
+        "updatedAt": "2019-11-01T10:58:01.000Z",
+        "ChefId": 3,
+        "Chef": {
+            "id": 3,
+            "firstName": "Sandy",
+            "lastName": "Sun",
+            "email": "test3@gmail.com",
+            "specialities": "Asian food",
+            "contact": "+14255536595",
+            "kitchenName": "Bamboo Garden",
+            "license": "WA-FOODLICENSE",
+            "address": "85 Pike St, Seattle, WA 98101",
+            "lat":"47.65",
+            "lng":"-122.31",
+            "createdAt": "2019-10-31T22:03:14.000Z",
+            "updatedAt": "2019-10-31T22:03:14.000Z",
+            "UserId": 4
+        }
+    }
+ ]
+
+export default onlineChefData;
