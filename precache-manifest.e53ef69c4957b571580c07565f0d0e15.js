dffea286644159793160927eb7773985a9dd6a89
Swathi <pottigariswathi@gmail.com>
2019-11-04 10:24:12 -0800

    Updates

---
+++ precache-manifest.e53ef69c4957b571580c07565f0d0e15.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ffac13d19eecdb71a235ebaff2621e1",
+    "url": "/OlafUI/index.html"
+  },
+  {
+    "revision": "11925d7be7b42e5e28a6",
+    "url": "/OlafUI/static/css/main.7dbf7db3.chunk.css"
+  },
+  {
+    "revision": "fe1c9822cfb129a544b3",
+    "url": "/OlafUI/static/js/2.6ec09fd5.chunk.js"
+  },
+  {
+    "revision": "11925d7be7b42e5e28a6",
+    "url": "/OlafUI/static/js/main.f926cdbe.chunk.js"
+  },
+  {
+    "revision": "5e7713a696f6a5334768",
+    "url": "/OlafUI/static/js/runtime-main.2d14a01c.js"
+  },
+  {
+    "revision": "95d3a774b87be507b0a1081baa76bfa3",
+    "url": "/OlafUI/static/media/carrot.95d3a774.svg"
+  },
+  {
+    "revision": "2b64a3d4533729f389bf650aadca3cb4",
+    "url": "/OlafUI/static/media/placeholder-foot.2b64a3d4.jpg"
+  },
+  {
+    "revision": "26787a6bd79a6e7868511d50fa7a622b",
+    "url": "/OlafUI/static/media/placeholder-head.26787a6b.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/SwathiPottigari/OlafU/precache-manifest.0a05e77c0a3900465e01bc1d09b1a8fd.js"
+  "/OlafUI/precache-manifest.e53ef69c4957b571580c07565f0d0e15.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SwathiPottigari/OlafU/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/OlafUI/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
