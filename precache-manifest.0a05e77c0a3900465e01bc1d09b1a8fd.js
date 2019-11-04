fa47e2d565528ac3b2b7657dadc515cd92d8b7e5
Swathi <pottigariswathi@gmail.com>
2019-11-04 10:22:09 -0800

    Updates

---
+++ precache-manifest.0a05e77c0a3900465e01bc1d09b1a8fd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a5a14cedfd5d020838c01fa9dc80b6b",
+    "url": "/SwathiPottigari/OlafU/index.html"
+  },
+  {
+    "revision": "088991d2659c2fbf6949",
+    "url": "/SwathiPottigari/OlafU/static/css/main.a8c93829.chunk.css"
+  },
+  {
+    "revision": "f364583d327181986cb8",
+    "url": "/SwathiPottigari/OlafU/static/js/2.414bba15.chunk.js"
+  },
+  {
+    "revision": "088991d2659c2fbf6949",
+    "url": "/SwathiPottigari/OlafU/static/js/main.19645ff2.chunk.js"
+  },
+  {
+    "revision": "8bc14431e7c39813b378",
+    "url": "/SwathiPottigari/OlafU/static/js/runtime-main.b9a07bee.js"
+  },
+  {
+    "revision": "95d3a774b87be507b0a1081baa76bfa3",
+    "url": "/SwathiPottigari/OlafU/static/media/carrot.95d3a774.svg"
+  },
+  {
+    "revision": "2b64a3d4533729f389bf650aadca3cb4",
+    "url": "/SwathiPottigari/OlafU/static/media/placeholder-foot.2b64a3d4.jpg"
+  },
+  {
+    "revision": "26787a6bd79a6e7868511d50fa7a622b",
+    "url": "/SwathiPottigari/OlafU/static/media/placeholder-head.26787a6b.jpg"
+  }
+]);
