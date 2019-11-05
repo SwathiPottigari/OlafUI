9f262fee6e110de64ca668982ba096b858ff853f
Andrew Weiss <wandrew8@gmail.com>
2019-11-04 21:39:51 -0800

    modal for ordering items added

---
+++ src/components/OrderItemModal/OrderItemModal.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './OrderItemModal.css';
+
+function OrderItemModal(props) {
+    return (
+        <div>
+            <div className="modal fade" id="orderItemModal">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header text-center">
+                            <h4 className="modal-title w-100 font-weight-bold">Please Confirm Your Order</h4>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body mx-3">
+                            <p className="order">You ordered {props.serving} serving(s) of {props.item}.</p>
+                            <p className="order">Your total comes to {props.total}!</p>
+                            <p className="order-details">To confirm your order, click submit below and the chef will be notified. You will recieve a notification text message shortly with address details to collect your food items.</p>
+                        </div>
+                        <div className="modal-footer d-flex justify-content-center">
+                            <button className="login-btn btn btn-info my-4 btn-block" data-dismiss="modal" type="submit">Submit Order</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default OrderItemModal;
